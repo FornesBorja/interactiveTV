238f076011dc461b33aac3f5225b8456e9b354cb
FornesBorja <fornes.borja@gmail.com>
2024-06-06 17:24:40 +0200

    series added, channel buttons now change channel

--- js/main.js
+++ js/main.js
@@ -4,30 +4,59 @@ let light = document.getElementsByClassName("light");
 let arrayLights = Array.from(light);
 let lightTV = document.getElementById("tv-on-light");
 let date_container = document.getElementById("date");
+let screen = document.getElementById("screen");
+let channels = document.getElementsByClassName("channel-button");
+channelsArray = Array.from(channels);
+const tv_power = document.getElementById("tv-power-button");
+const info = document.getElementById("info");
 
 const infrared = () => {
-    for (let i = 0; i < arrayLights.length; i++) {
-        arrayLights[i].classList.add("laser")
-
-        setTimeout(() => {
-            arrayLights[i].classList.remove("laser")
-        }, 200)
-    }
-}
-const dateToday = () => {
-    let date = new Date();
-    date_container.innerHTML = `${date}`;
+  for (let i = 0; i < arrayLights.length; i++) {
+    arrayLights[i].classList.add("laser");
     setTimeout(() => {
-        date_container.innerHTML = ` `;
+      arrayLights[i].classList.remove("laser");
+    }, 200);
+  }
+};
+const DateAndChannel = () => {
+  let date = new Date();
+  date_container.innerHTML = `${date}`;
+  setTimeout(() => {
+    date_container.innerHTML = ` `;
+  }, 3000);
+};
 
-    }, 3000)
+for (let i = 0; i < channelsArray.length; i++) {
+  channelsArray[i].addEventListener("click", () => {
+    if (is_on) {
+      let channel = `../imgs/channel-${i + 1}.gif`;
+      console.log(channel);
+      screen.style.backgroundImage = `url('../imgs/channel-${i + 1}.gif')`;
+      infrared();
+    }
+  });
+  
 }
 power.addEventListener("click", () => {
+  infrared();
+  is_on = !is_on;
+  lightTV.classList.toggle("on");
+  if (is_on) {
+    DateAndChannel();
+  }
+});
+tv_power.addEventListener("click", () => {
+  infrared();
+  is_on = !is_on;
+  lightTV.classList.toggle("on");
+  if (is_on) {
+    DateAndChannel();
+  }
+});
 
+info.addEventListener("click", () => {
+  if (is_on) {
+    DateAndChannel();
     infrared();
-    is_on = !is_on;
-    lightTV.classList.toggle("on");
-    is_on
-        ? dateToday()
-        : null;
+  }
 });
