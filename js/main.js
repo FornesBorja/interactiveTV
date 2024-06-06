2a2514dcf4bf7023b435ee41c6989bf32a86aaf6
“FornesBorja” <fornes.borja@gmail.com>
2024-06-06 19:22:17 +0200

    Refactor existing code, basic functionalities done

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 let is_on = false;
-let power = document.getElementById("remote-power-button");
-let light = document.getElementsByClassName("light");
+const power = document.getElementById("remote-power-button");
+const light = document.getElementsByClassName("light");
 let arrayLights = Array.from(light);
-let lightTV = document.getElementById("tv-on-light");
-let date_container = document.getElementById("date");
-let screen = document.getElementById("screen");
-let channels = document.getElementsByClassName("channel-button");
+const lightTV = document.getElementById("tv-on-light");
+const date_container = document.getElementById("date");
+const screen = document.getElementById("screen");
+const channels = document.getElementsByClassName("channel-button");
 channelsArray = Array.from(channels);
 const tv_power = document.getElementById("tv-power-button");
 const info = document.getElementById("info");
 const channel_container = document.getElementById("channel-container");
 let lastChannelIndex = -1; //Last channel used
 
+//Function to make the "led" shine
 const infrared = () => {
     for (let i = 0; i < arrayLights.length; i++) {
         arrayLights[i].classList.add("laser");
@@ -20,10 +21,30 @@ const infrared = () => {
         }, 200);
     }
 };
+
+//Switch on and off the TV
+const togglePower = () => {
+    infrared();
+    is_on = !is_on;
+    lightTV.classList.toggle("on");
+    if (is_on) {
+        screen.style.backgroundImage = `url('../imgs/static.gif')`;
+        //If it has a channed saved goes to that channel in 3 seconds
+        if (lastChannelIndex !== -1) {
+            setTimeout(() => {
+                screen.style.backgroundImage = `url('../imgs/channel-${lastChannelIndex + 1}.gif')`;
+                showDateAndChannel();
+            }, 3000);
+        }
+    } else {
+        screen.style.backgroundImage = ``;
+        screen.style.backgroundColor = `black`;
+    }
+};
+
 const showDate = () => {
     let date = new Date()
     date_container.innerHTML = `${date.toLocaleDateString()}  ${date.toLocaleTimeString()}`;
-
     setTimeout(() => {
         date_container.innerHTML = ` `;
     }, 3000);
@@ -36,45 +57,28 @@ const showChannel = () => {
         }, 3000);
     }
 };
+const showDateAndChannel = () => {
+    showDate();
+    showChannel();
+};
 
 for (let i = 0; i < channelsArray.length; i++) {
     channelsArray[i].addEventListener("click", () => {
         if (is_on) {
-            lastChannelIndex = i; 
+            lastChannelIndex = i;
             screen.style.backgroundImage = `url('../imgs/channel-${i + 1}.gif')`;
             infrared();
-            showChannel(); 
-
+            showChannel();
         }
     });
-
 }
-power.addEventListener("click", () => {
-    infrared();
-    is_on = !is_on;
-    lightTV.classList.toggle("on");
-    if (is_on) {
-        showDate();
-        screen.style.backgroundImage = `url('../imgs/static.gif')`;
-    } else {
-        screen.style.backgroundImage = ``;
-        screen.style.backgroundColor = `black`;
-    }
-});
 
-tv_power.addEventListener("click", () => {
-    infrared();
-    is_on = !is_on;
-    lightTV.classList.toggle("on");
-    if (is_on) {
-        showDate();
-    }
-});
+power.addEventListener("click", togglePower);
+tv_power.addEventListener("click", togglePower);
 
 info.addEventListener("click", () => {
     if (is_on) {
-        showDate();
-        showChannel();
+        showDateAndChannel();
         infrared();
     }
 });
