eda853f840e1181e4005bf27c479921bed8965be
“FornesBorja” <fornes.borja@gmail.com>
2024-06-06 18:20:26 +0200

    added info features (showDate and showChannel)

--- js/main.js
+++ js/main.js
@@ -9,54 +9,72 @@ let channels = document.getElementsByClassName("channel-button");
 channelsArray = Array.from(channels);
 const tv_power = document.getElementById("tv-power-button");
 const info = document.getElementById("info");
+const channel_container = document.getElementById("channel-container");
+let lastChannelIndex = -1; //Last channel used
 
 const infrared = () => {
-  for (let i = 0; i < arrayLights.length; i++) {
-    arrayLights[i].classList.add("laser");
-    setTimeout(() => {
-      arrayLights[i].classList.remove("laser");
-    }, 200);
-  }
+    for (let i = 0; i < arrayLights.length; i++) {
+        arrayLights[i].classList.add("laser");
+        setTimeout(() => {
+            arrayLights[i].classList.remove("laser");
+        }, 200);
+    }
 };
-const DateAndChannel = () => {
-  let date = new Date();
-  date_container.innerHTML = `${date}`;
-  setTimeout(() => {
-    date_container.innerHTML = ` `;
-  }, 3000);
+const showDate = () => {
+    let date = new Date()
+    date_container.innerHTML = `${date.toLocaleDateString()}  ${date.toLocaleTimeString()}`;
+
+    setTimeout(() => {
+        date_container.innerHTML = ` `;
+    }, 3000);
+};
+const showChannel = () => {
+    if (lastChannelIndex !== -1) {
+        channel_container.innerHTML = `Canal: ${lastChannelIndex + 1}`;
+        setTimeout(() => {
+            channel_container.innerHTML = ` `;
+        }, 3000);
+    }
 };
 
 for (let i = 0; i < channelsArray.length; i++) {
-  channelsArray[i].addEventListener("click", () => {
-    if (is_on) {
-      let channel = `../imgs/channel-${i + 1}.gif`;
-      console.log(channel);
-      screen.style.backgroundImage = `url('../imgs/channel-${i + 1}.gif')`;
-      infrared();
-    }
-  });
-  
+    channelsArray[i].addEventListener("click", () => {
+        if (is_on) {
+            lastChannelIndex = i; 
+            screen.style.backgroundImage = `url('../imgs/channel-${i + 1}.gif')`;
+            infrared();
+            showChannel(); 
+
+        }
+    });
+
 }
 power.addEventListener("click", () => {
-  infrared();
-  is_on = !is_on;
-  lightTV.classList.toggle("on");
-  if (is_on) {
-    DateAndChannel();
-  }
+    infrared();
+    is_on = !is_on;
+    lightTV.classList.toggle("on");
+    if (is_on) {
+        showDate();
+        screen.style.backgroundImage = `url('../imgs/static.gif')`;
+    } else {
+        screen.style.backgroundImage = ``;
+        screen.style.backgroundColor = `black`;
+    }
 });
+
 tv_power.addEventListener("click", () => {
-  infrared();
-  is_on = !is_on;
-  lightTV.classList.toggle("on");
-  if (is_on) {
-    DateAndChannel();
-  }
+    infrared();
+    is_on = !is_on;
+    lightTV.classList.toggle("on");
+    if (is_on) {
+        showDate();
+    }
 });
 
 info.addEventListener("click", () => {
-  if (is_on) {
-    DateAndChannel();
-    infrared();
-  }
+    if (is_on) {
+        showDate();
+        showChannel();
+        infrared();
+    }
 });
